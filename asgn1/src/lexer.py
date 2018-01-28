ca1319418daf9e8e1d068e6e79fb564d43a82230
gowtham <v.g.prudhvi38@gmail.com>
2018-01-28 21:00:15 +0530

    add more testcases

--- asgn1/src/lexer.py
+++ asgn1/src/lexer.py
@@ -3,16 +3,16 @@ import sys
 
 file_location=sys.argv[1]
 
-#token specification
+#token declaration
 keywords=['BEGIN','class','ensure','nil','self','when','END','def','false','not','super','while','alias','defined','for','or','then','yield','and','do','if','redo','true','begin','else','in','rescue','undef','break','elsif','module','retry','unless','case','end','next','return','until']
 operators=['CONSTANT_RESOLUTION','ELEMENT_REFERENCE','ELEMENT_SET','POWER','UNARY_MINUS','UNARY_PLUS','SYMBOL_NOT','COMPLEMENT','MULTIPLY','DIVIDE','MODULO','PLUS','MINUS','LEFT_SHIFT','RIGHT_SHIFT','BIT_AND','BIT_OR','BIT_XOR','GREATER','GREATER_EQUALS','LESS','LESS_EQUALS','COMPARISON','DOUBLE_EQUALS','TRIPLE_EQUALS','NOT_EQUALS','EQUAL_TILDE','BANG_TILDE','LOGICAL_AND','LOGICAL_OR','INCL_RANGE','EXCL_RANGE','EQUALS','MODULO_EQUALS','DIVIDE_EQUALS','MINUS_EQUALS','PLUS_EQUALS','OR_EQUALS','AND_EQUALS','RIGHT_SHIFT_EQUALS','LEFT_SHIFT_EQUALS','MULTIPLY_EQUALS','LOGICAL_AND_EQUALS','LOGICAL_OR_EQUALS','POWER_EQUALS','WORD_NOT','WORD_AND','WORD_OR','MAP','PLUS_AT','MINUS_AT']
-tokens=keywords+operators+['IDENTIFIER','FLOAT','NUMBER','GLOBAL','STRING','STRING2','HEREDOC','REGEXP','DOUBLE_QUOTE','DOLLAR','COLON','QUESTION_MARK','OPEN_BRACKET','CLOSE_BRACKET','OPEN_FLOWER','CLOSE_FLOWER','OPEN_SQUARE','CLOSE_SQUARE','COMMA','DOT','SEMI_COLON','AT_THE_RATE']
+tokens=keywords+operators+['IDENTIFIER','FLOAT','INT','GLOBAL','STRING','STRING2','HEREDOC','REGEXP','DOUBLE_QUOTE','DOLLAR','COLON','QUESTION_MARK','OPEN_BRACKET','CLOSE_BRACKET','OPEN_FLOWER','CLOSE_FLOWER','OPEN_SQUARE','CLOSE_SQUARE','COMMA','DOT','SEMI_COLON','AT_THE_RATE']
 
 reserved={}
 
 for word in keywords:
 	reserved[word]=word
-
+#token specification
 t_CONSTANT_RESOLUTION=r'::'
 t_ELEMENT_REFERENCE=r'\[\]'
 t_ELEMENT_SET=r'\[\]='
@@ -79,7 +79,7 @@ def t_FLOAT(t):
 		t.value = 0
 	return t
 
-def t_NUMBER(t):
+def t_INT(t):
 	r'\d+'
 	try:
 		t.value = int(t.value)
@@ -89,7 +89,7 @@ def t_NUMBER(t):
 	return t
 
 t_GLOBAL=r'(\$[a-zA-Z_][a-zA-Z_0-9_]*)|(\$\-.)|(\$.)'
-t_STRING=r'\".*\"|\'.*\''
+t_STRING=r'\'([^\\\']+|\\.)*\' | \"([^\\\"]+|\\.)*\"'
 t_STRING2=r'%(Q|q|x)..*.'
 t_HEREDOC=r'<<([a-zA-Z_][a-zA-Z_0-9_]*|(\".*\"|\'.*\'))\n.*\n[a-zA-Z_][a-zA-Z_0-9_]*'
 t_REGEXP=r'(\/.*\/([iop])?)|(%r..*.)'
@@ -140,6 +140,5 @@ while True:
 				tok_dict[tok.type][0]+=1
 		else:
 			tok_dict[tok.type]=[1,[tok.value]]
-			# print tok.type,tok.value
 for tok in tok_dict.keys():
 	print(tok,tok_dict[tok][0],tok_dict[tok][1])
