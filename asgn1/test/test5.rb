bd6267b8db508d2c2e0f353403b40f21734e987a
gowtham <v.g.prudhvi38@gmail.com>
2018-01-28 21:31:03 +0530

    final check

--- asgn1/test/test5.rb
+++ asgn1/test/test5.rb
@@ -2,6 +2,17 @@
 if defined x
 	x[]=z
 end
+
+if rno > 50 
+    print "That's more than half as big as it could be!\n"
+elsif rno == 42
+    print "That's the ultimate magic number!!!!\n"
+elsif rno < 10
+    print "That's pretty small, actually.\n"
+else
+    print "What a boring number.\n"
+end
+
 comp1=1==1
 comp2=1==2
 comp = comp1 and comp2
