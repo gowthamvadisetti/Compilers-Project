8de0eb0cd00b2f5a4a73761e708fb5a57dcafb91
Amarnath Chakala <personal@Amarnaths-MacBook-Pro.local>
2018-02-04 23:41:39 +0530

    small corrections

--- asgn2/mips/test1.asm
+++ asgn2/mips/test1.asm
@@ -1,5 +1,5 @@
 .data
-bb: .word 0
+aa: .word 0
 .text
 main:
 line1: 
@@ -9,9 +9,9 @@ line2:
 li $t1,1
 add $t0,$t0,$t1
 line3: 
-li $t1,2
-ble $t0,$t1,line2
-line4: 
 li $v0,1
 move $a0,$t0
 syscall
+line4: 
+li $t1,100
+ble $t0,$t1,line2
