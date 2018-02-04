6629ff2accf4819b02c524909e06fb57946f9192
gowtham <v.g.prudhvi38@gmail.com>
2018-02-04 14:27:28 +0530

    add code after end of block

--- asgn2/mips/test1.asm
+++ asgn2/mips/test1.asm
@@ -11,13 +11,18 @@ li $v0,5
 syscall
 move $t0,$v0
 li $t1,2
-add $t2,$t0,$t1
-add $t3,$t2,$t0
-add $t4,$t3,$t2
-add $t5,$t4,$t2
+add $t1,$t0,$t1
+add $t2,$t1,$t0
+add $t3,$t2,$t1
+add $t4,$t3,$t1
 li $v0,1
-move $a0,$t5
+move $a0,$t4
 syscall
 li $v0,1
-move $a0,$t3
+move $a0,$t2
 syscall
+sw $t3,e
+sw $t2,d
+sw $t1,c
+sw $t0,a
+sw $t4,f
