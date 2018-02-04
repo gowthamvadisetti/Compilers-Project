b3a5389ab84763add1b79b48ec33083ac4ce305f
gowtham <v.g.prudhvi38@gmail.com>
2018-02-04 23:01:48 +0530

    correct ifgoto

--- asgn2/mips/test1.asm
+++ asgn2/mips/test1.asm
@@ -1,18 +1,17 @@
 .data
-a: .word 0
+bb: .word 0
 .text
 main:
-line1: li $t0,40
+line1: 
+li $t0,1
 move $t0,$t0
-sw $t0,a
-line2: lw $t0,a
-li $t1,5
+line2: 
+li $t1,1
 add $t0,$t0,$t1
-sw $t0,a
-line2: lw $t0,a
-li $t1,50
+line3: 
+li $t1,2
 ble $t0,$t1,line2
-line3: li $v0,1
+line4: 
+li $v0,1
 move $a0,$t0
 syscall
-sw $t0,a
