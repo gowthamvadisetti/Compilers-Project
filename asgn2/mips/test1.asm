5f1810d0f58dffba2f7bbe642396555c6e2aec8c
Amarnath Chakala <personal@Amarnaths-MacBook-Pro.local>
2018-02-06 00:21:24 +0530

    instruction manual??

--- asgn2/mips/test1.asm
+++ asgn2/mips/test1.asm
@@ -1,38 +1,46 @@
 .data
 aa: .word 0
 bb: .word 0
-cc: .word 0
-dd: .word 0
+str1: .asciiz "enter any number\n"
+str10: .asciiz "factorial of input is "
 .text
 main:
 line1: 
-li $t0,1
-move $t0,$t0
+la $a0,str1
+li $v0,4
+syscall
 line2: 
-li $t1,0
-move $t1,$t1
+lw $t0,aa
+li $v0,5
+syscall
+move $t0,$v0
 line3: 
-li $t2,0
-move $t2,$t2
+li $t1,1
+move $t1,$t1
 line4: 
-li $t3,0
-move $t3,$t3
-line5: 
-add $t3,$t0,$t1
+jal fact
+fact:
 line6: 
-move $t1,$t0
+mult $t1,$t0
+mflo $t1
 line7: 
-move $t0,$t3
+li $t2,1
+sub $t0,$t0,$t2
 line8: 
-li $t4,1
-add $t2,$t2,$t4
+li $t2,1
+ble $t0,$t2,line10
 line9: 
-li $t4,10
-ble $t2,$t4,line5
+jal fact
+line9: 
+jal fact
 line10: 
-li $v0,1
-move $a0,$t0
+la $a0,str10
+li $v0,4
 syscall
 line11: 
+li $v0,1
+move $a0,$t1
+syscall
+line12: 
 li $v0,10
 syscall
