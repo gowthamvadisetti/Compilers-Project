adfd2f66381806925e4c3d517231d50a16fa3661
gowtham <v.g.prudhvi38@gmail.com>
2018-02-05 20:24:33 +0530

    add Makefile

--- asgn2/mips/test1.asm
+++ asgn2/mips/test1.asm
@@ -1,21 +1,17 @@
 .data
 a: .word 0
-c: .word 0
 .text
 main:
 line1: 
-li $t0,2
+li $t0,40
 move $t0,$t0
 line2: 
-nor $t1,$t0,$t0
+li $t1,5
+add $t0,$t0,$t1
 line3: 
-li $v0,1
-move $a0,$t0
-syscall
+li $t1,50
+ble $t0,$t1,line2
 line4: 
 li $v0,1
-move $a0,$t1
-syscall
-line5: 
-li $v0,10
+move $a0,$t0
 syscall
