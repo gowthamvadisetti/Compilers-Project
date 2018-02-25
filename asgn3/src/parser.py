f73ab253ae93ef8201025cabe94a279bde462c44
gowtham <v.g.prudhvi38@gmail.com>
2018-02-25 11:43:26 +0530

    display output as tree

--- asgn3/src/parser.py
+++ asgn3/src/parser.py
@@ -1,17 +1,24 @@
 import ply.yacc as yacc
-
+from anytree import Node, RenderTree
 # Get the token map from the lexer.  This is required.
 import lexer
 import sys
 tokens=lexer.tokens
-
+def createTree(root,tuple_part):
+    curr=Node("",parent=root)
+    for i in tuple_part:
+        if type(i) is tuple:
+            createTree(curr,i)
+        else:
+            endnode=Node(i,parent=curr)
+    return
 file_location=sys.argv[1]
 
 def p_stmt(p):
     '''stmt : stmt if expr
         | stmt while expr
         | expr'''
-    p[0]=('expr',p[1])
+    p[0]=(p[1])
 def p_expr_plus(p):
     'expr : expr PLUS term'
     p[0] = tuple(p[1:])
@@ -22,7 +29,6 @@ def p_expr_minus(p):
 
 def p_expr_term(p):
     'expr : term'
-    print p[1:]
     p[0]=(p[1])
 
 def p_term_factor(p):
@@ -32,8 +38,7 @@ def p_term_factor(p):
 
 def p_factor_num(p):
     'factor : NUMBER'
-    print p[1:]
-    p[0] = ('NUM',p[1])
+    p[0] = (p[1])
 
 
 # Error rule for syntax errors
@@ -46,4 +51,8 @@ parser = yacc.yacc()
 fp=open(file_location,'r')
 file_contents=fp.read()
 t=yacc.parse()
+root = Node("root")
+createTree(root,t)
+for pre, fill, node in RenderTree(root):
+    print("%s%s" % (pre, node.name))
 print(t)
--- asgn3/src/tree.py
+++ asgn3/src/tree.py
@@ -3,15 +3,11 @@ from anytree import Node, RenderTree
 udo = Node("Udo")
 marc = Node("Marc", parent=udo)
 lian = Node("Lian", parent=marc)
-dan = Node("Dan", parent=udo)
-jet = Node("Jet", parent=dan)
-jan = Node("Jan", parent=dan)
-joe = Node("Joe", parent=dan)
+marc = Node("Dan", parent=udo)
+jet = Node("Jet", parent=marc)
+jan = Node("Jan", parent=marc)
+joe = Node("Joe", parent=marc)
 
-print(udo)
-Node('/Udo')
-print(joe)
-Node('/Udo/Dan/Joe')
 
 for pre, fill, node in RenderTree(udo):
     print("%s%s" % (pre, node.name))
