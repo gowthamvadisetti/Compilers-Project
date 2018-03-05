28c5c9831dfd3bb76ce887c080036f8bab91705c
Amarnath Chakala <c.amarnath6789@gmail.com>
2018-03-05 21:30:00 +0530

    add node name to parse tree

--- asgn3/src/parser.py
+++ asgn3/src/parser.py
@@ -5,7 +5,12 @@ import lexer
 import sys
 tokens=lexer.tokens
 def createTree(root,tuple_part):
-    curr=Node("",parent=root)
+    if type(tuple_part) is tuple:
+        node_name=tuple_part[0]
+        tuple_part=tuple_part[1:]
+    else:
+        node_name=""
+    curr=Node(node_name,parent=root)
     if type(tuple_part) is tuple:
         for i in tuple_part:
             if type(i) is tuple:
@@ -14,9 +19,9 @@ def createTree(root,tuple_part):
                 endnode=Node(i,parent=curr)
     return
 
-def getRule(p):
+def getRule(p,node_name):
     if len(p)>2:
-        p[0] = tuple(p[1:])
+        p[0] = tuple([node_name]+p[1:])
     else:
         p[0] = (p[1])
 
@@ -28,7 +33,7 @@ def p_compstmt(p):
                 | stmt newline expr
                 | stmt newline expr newline
     '''
-    getRule(p)
+    getRule(p,'compstmt')
 
 def p_stmt(p):
     '''stmt : call do BIT_OR blockvar BIT_OR compstmt end
@@ -48,7 +53,7 @@ def p_stmt(p):
             | lhs EQUALS command
             | expr
     '''
-    getRule(p)
+    getRule(p,'stmt')
 
 def p_expr(p):
     '''expr : mlhs EQUALS mrhs
@@ -61,13 +66,13 @@ def p_expr(p):
             | SYMBOL_NOT command
             | arg
     '''
-    getRule(p)
+    getRule(p,'expr')
 
 def p_call(p):
     '''call : function
             | command
     '''
-    getRule(p)
+    getRule(p,'call')
 
 def p_command(p):
     '''command : operation callargs
@@ -75,7 +80,7 @@ def p_command(p):
                | primary CONSTANT_RESOLUTION operation callargs
                | super callargs
     '''
-    getRule(p)
+    getRule(p,'command')
 
 
 def p_function(p):
@@ -92,27 +97,27 @@ def p_function(p):
                 | super OPEN_BRACKET CLOSE_BRACKET
                 | super
     '''
-    getRule(p)
+    getRule(p,'function')
 
 def p_arg_equals(p):
     '''arg : lhs EQUALS arg
            | lhs opasgn arg
            | arg
     '''
-    getRule(p)
+    getRule(p,'arg')
 
 def p_arg_range(p):
     '''arg : term1 INCL_RANGE term1
             | term1 EXCL_RANGE term1
             | term1
     '''
-    getRule(p)
+    getRule(p,'arg')
 
 def p_term1(p):
     '''term1 : term2 DOUBLE_EQUALS term2
             | term2
     '''
-    getRule(p)
+    getRule(p,'term1')
 
 def p_term2(p):
     '''term2 : term2 LESS term3
@@ -121,14 +126,14 @@ def p_term2(p):
             | term2 GREATER_EQUALS term3
             | term3
     '''
-    getRule(p)
+    getRule(p,'term2')
 
 def p_term3(p):
     '''term3 : term3 PLUS term4
             | term3 MINUS term4
             | term4
     '''
-    getRule(p)
+    getRule(p,'term3')
 
 def p_term4(p):
     '''term4 : term4 MULTIPLY term5
@@ -136,76 +141,76 @@ def p_term4(p):
             | term4 MODULO term5
             | term5
     '''
-    getRule(p)
+    getRule(p,'term4')
 
 def p_term5(p):
     '''term5 : MINUS term5
             | term6
     '''
-    getRule(p)
+    getRule(p,'term5')
 
 def p_term6(p):
     '''term6 : PLUS term6
             | primary
     '''
-    getRule(p)
+    getRule(p,'term6')
 
 def p_primary(p):
     '''primary : OPEN_BRACKET compstmt CLOSE_BRACKET
-        | literal
-        | variable
-        | primary CONSTANT_RESOLUTION IDENTIFIER
-        | CONSTANT_RESOLUTION IDENTIFIER
-        | primary OPEN_SQUARE args CLOSE_SQUARE
-        | primary OPEN_SQUARE CLOSE_SQUARE
-        | OPEN_SQUARE args COMMA CLOSE_SQUARE
-        | OPEN_SQUARE args CLOSE_SQUARE
-        | OPEN_SQUARE CLOSE_SQUARE
-        | OPEN_FLOWER args COMMA CLOSE_FLOWER
-        | OPEN_FLOWER args CLOSE_FLOWER
-        | OPEN_FLOWER CLOSE_FLOWER
-        | OPEN_FLOWER assocs COMMA CLOSE_FLOWER
-        | OPEN_FLOWER assocs CLOSE_FLOWER
-        | return OPEN_BRACKET callargs CLOSE_BRACKET
-        | return OPEN_BRACKET CLOSE_BRACKET
-        | return
-        | yield OPEN_BRACKET callargs CLOSE_BRACKET
-        | yield OPEN_BRACKET CLOSE_BRACKET
-        | yield
-        | defined OPEN_BRACKET arg CLOSE_BRACKET
-        | function
-        | function OPEN_FLOWER BIT_OR blockvar BIT_OR compstmt CLOSE_FLOWER
-        | function OPEN_FLOWER BIT_OR BIT_OR compstmt CLOSE_FLOWER
-        | function OPEN_FLOWER compstmt CLOSE_FLOWER
-        | if expr pthen compstmt multelsif else compstmt end
-        | if expr pthen compstmt end
-        | unless expr pthen compstmt else compstmt end
-        | unless expr pthen compstmt end
-        | while expr pdo compstmt end
-        | until expr pdo compstmt end
-        | case compstmt multcase else compstmt end
-        | case compstmt multcase end
-        | for blockvar in expr pdo compstmt end
-        | class IDENTIFIER LESS IDENTIFIER compstmt end
-        | class IDENTIFIER compstmt end
-        | module IDENTIFIER compstmt end
-        | def fname argdecl compstmt end
-        | def singleton DOT fname argdecl compstmt end
-        | def singleton CONSTANT_RESOLUTION fname argdecl compstmt end
+            | literal
+            | variable
+            | primary CONSTANT_RESOLUTION IDENTIFIER
+            | CONSTANT_RESOLUTION IDENTIFIER
+            | primary OPEN_SQUARE args CLOSE_SQUARE
+            | primary OPEN_SQUARE CLOSE_SQUARE
+            | OPEN_SQUARE args COMMA CLOSE_SQUARE
+            | OPEN_SQUARE args CLOSE_SQUARE
+            | OPEN_SQUARE CLOSE_SQUARE
+            | OPEN_FLOWER args COMMA CLOSE_FLOWER
+            | OPEN_FLOWER args CLOSE_FLOWER
+            | OPEN_FLOWER CLOSE_FLOWER
+            | OPEN_FLOWER assocs COMMA CLOSE_FLOWER
+            | OPEN_FLOWER assocs CLOSE_FLOWER
+            | return OPEN_BRACKET callargs CLOSE_BRACKET
+            | return OPEN_BRACKET CLOSE_BRACKET
+            | return
+            | yield OPEN_BRACKET callargs CLOSE_BRACKET
+            | yield OPEN_BRACKET CLOSE_BRACKET
+            | yield
+            | defined OPEN_BRACKET arg CLOSE_BRACKET
+            | function
+            | function OPEN_FLOWER BIT_OR blockvar BIT_OR compstmt CLOSE_FLOWER
+            | function OPEN_FLOWER BIT_OR BIT_OR compstmt CLOSE_FLOWER
+            | function OPEN_FLOWER compstmt CLOSE_FLOWER
+            | if expr pthen compstmt multelsif else compstmt end
+            | if expr pthen compstmt end
+            | unless expr pthen compstmt else compstmt end
+            | unless expr pthen compstmt end
+            | while expr pdo compstmt end
+            | until expr pdo compstmt end
+            | case compstmt multcase else compstmt end
+            | case compstmt multcase end
+            | for blockvar in expr pdo compstmt end
+            | class IDENTIFIER LESS IDENTIFIER compstmt end
+            | class IDENTIFIER compstmt end
+            | module IDENTIFIER compstmt end
+            | def fname argdecl compstmt end
+            | def singleton DOT fname argdecl compstmt end
+            | def singleton CONSTANT_RESOLUTION fname argdecl compstmt end
     '''
-    getRule(p)
+    getRule(p,'primary')
 
 def p_multcase(p):
     '''multcase : when whenargs pthen compstmt multcase
                 | when whenargs pthen compstmt
     '''
-    getRule(p)
+    getRule(p,'multcase')
 
 def p_multelsif(p):
     '''multelsif : elsif expr pthen compstmt multelsif
                  | empty
     '''
-    getRule(p)
+    getRule(p,'multelsif')
 
 def p_literal(p):
     '''literal : NUMBER
@@ -215,20 +220,20 @@ def p_literal(p):
                | HEREDOC
                | REGEXP
     '''
-    getRule(p)
+    getRule(p,'literal')
 
 def p_blockvar(p):
     '''blockvar : lhs
                 | mlhs
     '''
-    getRule(p)
+    getRule(p,'blockvar')
 
 def p_whenargs(p):
     '''whenargs : args COMMA MULTIPLY arg
                 | args
                 | MULTIPLY arg
     '''
-    getRule(p)
+    getRule(p,'whenargs')
 
 def p_mlhs(p):
     '''mlhs : mlhsitem COMMA mlhsitem multmlhs MULTIPLY lhs
@@ -238,19 +243,19 @@ def p_mlhs(p):
             | mlhsitem COMMA MULTIPLY
             | mlhsitem COMMA
     '''
-    getRule(p)
+    getRule(p,'mlhs')
 
 def p_multmlhs(p):
     '''multmlhs : COMMA mlhsitem multmlhs
                 | empty
     '''
-    getRule(p)
+    getRule(p,'multmlhs')
 
 def p_mlhsitem(p):
     '''mlhsitem : lhs
                 | OPEN_BRACKET mlhs CLOSE_BRACKET
     '''
-    getRule(p)
+    getRule(p,'mlhsitem')
 
 def p_lhs(p):
     '''lhs : variable
@@ -258,14 +263,14 @@ def p_lhs(p):
            | primary OPEN_SQUARE CLOSE_SQUARE
            | primary DOT IDENTIFIER 
     '''
-    getRule(p)
+    getRule(p,'lhs')
 
 def p_mrhs(p):
     '''mrhs : args
             | args COMMA MULTIPLY arg
             | MULTIPLY arg
     '''
-    getRule(p)
+    getRule(p,'mrhs')
 
 def p_callargs(p):
     '''callargs : args
@@ -284,24 +289,24 @@ def p_callargs(p):
                 | BIT_AND arg
                 | command
     '''
-    getRule(p)
+    getRule(p,'callargs')
 
 def p_args(p):
     '''args : arg multargs
     '''
-    getRule(p)
+    getRule(p,'args')
 
 def p_multargs(p):
     '''multargs : COMMA arg multargs
                 | empty
     '''
-    getRule(p)
+    getRule(p,'multargs')
 
 def p_argdecl(p):
     '''argdecl : OPEN_BRACKET arglist CLOSE_BRACKET
                | arglist newline
     '''
-    getRule(p)
+    getRule(p,'argdecl')
 
 def p_arglist(p):
     '''arglist : IDENTIFIER multarglist COMMA MULTIPLY IDENTIFIER COMMA BIT_AND IDENTIFIER
@@ -315,56 +320,56 @@ def p_arglist(p):
                | BIT_AND IDENTIFIER
                | empty
     '''
-    getRule(p)
+    getRule(p,'arglist')
 
 def p_multarglist(p):
     '''multarglist : COMMA IDENTIFIER multarglist
                  | empty
     '''
-    getRule(p)
+    getRule(p,'multarglist')
 
 def p_singleton(p):
     '''singleton : variable
                | OPEN_BRACKET expr CLOSE_BRACKET
     '''
-    getRule(p)
+    getRule(p,'singleton')
 
 def p_assocs(p):
     '''assocs : assoc multassocs
     '''
-    getRule(p)
+    getRule(p,'assocs')
 
 def p_multassocs(p):
     '''multassocs : COMMA assoc multassocs
                   | empty
     '''
-    getRule(p)
+    getRule(p,'multassocs')
 
 def p_assoc(p):
     '''assoc : arg MAP arg
     '''
-    getRule(p)
+    getRule(p,'assoc')
 
 def p_variable(p):
     '''variable : varname
                 | nil
                 | self
     '''
-    getRule(p)
+    getRule(p,'variable')
 
 def p_pthen(p):
     '''pthen : newline
              | then
              | newline then
     '''
-    getRule(p)
+    getRule(p,'pthen')
 
 def p_pdo(p):
     '''pdo : newline
            | do
            | newline do
     '''
-    getRule(p)
+    getRule(p,'pdo')
 
 def p_opasgn(p):
     '''opasgn : MODULO_EQUALS
@@ -381,12 +386,12 @@ def p_opasgn(p):
               | LOGICAL_OR_EQUALS
               | POWER_EQUALS
     '''
-    getRule(p)
+    getRule(p,'opasgn')
 def p_symbol(p):
     '''symbol : COLON fname
               | COLON varname
     '''
-    getRule(p)
+    getRule(p,'symbol')
 
 def p_fname(p):
     '''fname : IDENTIFIER
@@ -416,27 +421,27 @@ def p_fname(p):
              | ELEMENT_REFERENCE
              | ELEMENT_SET
     '''
-    getRule(p)
+    getRule(p,'fname')
 
 def p_operation(p):
     '''operation  : IDENTIFIER
                 | IDENTIFIER SYMBOL_NOT
                 | IDENTIFIER QUESTION_MARK
     '''
-    getRule(p)
+    getRule(p,'operation')
 
 def p_varname(p):
     '''varname : GLOBAL 
               | AT_THE_RATE IDENTIFIER
               | IDENTIFIER
     '''
-    getRule(p)
+    getRule(p,'varname')
 
 def p_newline(p):
     '''newline : SEMI_COLON
                | NEWLINE 
     '''
-    getRule(p)
+    getRule(p,'newline')
 
 
 #For epsilon definitions
