eae495740300c1890b0f77c04bb42a9736c9bf11
Jaswanth_Chandra <jsjchandra@gmail.com>
2018-03-09 01:58:11 +0530

    minor changes

--- asgn3/demo/dangling_if_else.rb
+++ asgn3/demo/dangling_if_else.rb
@@ -1,22 +1,10 @@
-#!/usr/bin/ruby -w
-
 i = 0
 a = [1, 2, 3]
-
-
 if i <= 3
 	a[i] += 1
 end
-
-
 if i >= 2
 	a[i] -= 1
-
 else
 	a[i] = 1
-end
-
-
-
-
-	
+end	
--- asgn3/demo/switch.rb
+++ asgn3/demo/switch.rb
@@ -1,35 +1,14 @@
 #!/usr/bin/ruby -w
-
 wflg, tflg = 0, 0
 dflg = 0
 c = 'W'
-
-
 case c
     when 'w'
-    
     when 'W'
         wflg = 1
-
     when 't'
-
     when 'T'
         tflg = 1       
-
     when 'W'
         dflg = 1
-        
-
 end
-
-
-
-
-
-
-
-
-
-
-
-
