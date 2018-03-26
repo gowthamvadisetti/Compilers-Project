f42a7c02833f907b9c6baf1d8505d47b52ca8364
Amarnath Chakala <personal@Amarnaths-MacBook-Pro.local>
2018-03-26 09:36:13 +0530

    test cases added

---
+++ asgn4/test_assgn4/logical.rb
@@ -0,0 +1,14 @@
+a=0
+b=1
+c=a|b
+print "output of 0 or 1  is"
+print (c.to_i),"\n"
+c=a&b
+print "output of 0 and 1  is"
+print (c.to_i),"\n"
+c=a^b
+print "output of 0 xor 1  is"
+print (c.to_i),"\n"
+c=~(4|5)
+print "output of 4 nor 5  is"
+print (c.to_i),"\n"
---
+++ asgn4/test_assgn4/or.rb
@@ -0,0 +1,5 @@
+a=2
+b=7
+c=a|b
+print "output of 2 or 7  is"
+print (c.to_i),"\n"
