2a11f86941669050165e021a253ce07cce57aecc
Gowtham Prudhvi <v.g.prudhvi38@gmail.com>
2018-04-05 19:08:33 +0530

    minor

--- project/mips/test1.asm
+++ project/mips/test1.asm
@@ -1,20 +1,21 @@
 .data
 fact_n2: .word 0
 fact_t1: .word 0
+fact_t5: .word 0
 num2: .word 0
 fact_t3: .word 0
 fact_t2: .word 0
+dd: .word 0
 fact_temp4: .word 0
 fact_t0: .word 0
 num: .word 0
 fact_a: .word 0
 fact_t4: .word 0
 fact_n: .word 0
-dd: .word 0
 .text
 main:
 li $t0,5
-li $t1,10
+li $t1,1
 move $a0,$t0
 move $a1,$t1
 sub $sp, $sp,12
@@ -55,21 +56,25 @@ l3:
 li $t6,1
 sub $t6,$t3,$t6
 move $t7,$t6
+li $t8,1
+add $t8,$t4,$t8
+move $t4,$t8
 move $a0,$t7
 move $a1,$t4
 sub $sp, $sp,12
 sw $a0,4($sp)
 jal fact
 sw $v0,fact_a
-li $t8,1
-add $t8,$t3,$t8
-move $t3,$t8
-mult $t3,$t4
-mflo $t9
-lw $s0,fact_a
-mult $t9,$s0
-mflo $s1
-move $v0,$s1
+li $t9,1
+sub $t9,$t4,$t9
+move $t4,$t9
+li $s0,1
+add $s0,$t3,$s0
+move $t3,$s0
+lw $s1,fact_a
+mult $t4,$s1
+mflo $s2
+move $v0,$s2
 lw $ra,0($sp)
 sw $v0,8($sp)
 addi $sp,$sp,12
