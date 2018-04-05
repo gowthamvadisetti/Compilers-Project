5621741a21c63e3e1d8bd649a49003948a56bf4e
Jaswanth_Chandra <jsjchandra@gmail.com>
2018-04-05 22:03:06 +0530

    Add test case

--- project/mips/test1.asm
+++ project/mips/test1.asm
@@ -1,17 +1,35 @@
 .data
+a: .word 0
 f_a: .word 0
+g_a: .word 0
 .text
 main:
+li $t0,22
 sub $sp, $sp,12
 sw $a0,4($sp)
 jal f
+li $v0,1
+move $a0,$t0
+syscall
 li $v0,10
 syscall
 f:
 sw $ra,0($sp)
-li $t0,1
+li $t1,1
+sub $sp, $sp,12
+sw $a0,4($sp)
+jal g
 li $v0,1
-move $a0,$t0
+move $a0,$t1
+syscall
+lw $ra,0($sp)
+addi $sp,$sp,12
+jr $ra
+g:
+sw $ra,0($sp)
+li $t2,45
+li $v0,1
+move $a0,$t2
 syscall
 lw $ra,0($sp)
 addi $sp,$sp,12
---
+++ project/test/new27.rb
@@ -0,0 +1,12 @@
+def f()
+	a = 1
+	g()
+	print (a)
+end
+def g()
+	a = 45
+	print (a)
+end
+a = 22
+f()
+print (a)
