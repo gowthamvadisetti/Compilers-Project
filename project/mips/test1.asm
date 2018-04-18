5bab2330f256a8697c7604fa9918fd18c6dbf38c
Jaswanth_Chandra <jsjchandra@gmail.com>
2018-04-19 00:29:18 +0530

    Correct bug in exclusive for

--- project/mips/test1.asm
+++ project/mips/test1.asm
@@ -1,32 +1,32 @@
 .data
-jj: .word 0
+a: .word 0
+c: .word 0
 t0: .word 0
 t1: .word 0
-t3: .word 0
 t2: .word 0
+str14: .asciiz "1\n"
 .text
 main:
-li $t0,0
-li $t1,0
-li $t2,5
-move $t3,$t1
+li $t0,1
+li $t1,10
+li $t2,1
+add $t2,$t0,$t2
+li $t3,1
+sub $t3,$t1,$t3
+move $t4,$t2
 l0:
-bge $t3,$t1,l3
+bge $t4,$t2,l3
 j l2
 l3:
-ble $t3,$t2,l1
+ble $t4,$t3,l1
 j l2
 l1:
-li $t4,1
-add $t4,$t0,$t4
-move $t0,$t4
+la $a0,str14
+li $v0,4
+syscall
 li $t5,1
-add $t3,$t3,$t5
+add $t4,$t4,$t5
 j l0
 l2:
-li $v0,1
-move $a0,$t0
-syscall
-li $t0,10
 li $v0,10
 syscall
